cefa270fc72410e7b16fc8d7d68c637cb17d2903
cauexmx <127966350+cauexmx@users.noreply.github.com>
2023-08-02 13:16:32 -0300

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,13 @@
+let myAge = 16; //Armazena minha idade em anos
+let earlyYears = 2; //Dois primeiros anos de vida do cachorro
+earlyYears *=10.5; //multipliquei 10.5 na variavél early years
+let laterYears = myAge - 2; //tirei -2 de myage e atribui para lateryears
+laterYears *=  4 ; //multipliquei later years por 4
+console.log(laterYears)//printei lateryears
+console.log(earlyYears)//printei earlyyears
+let myAgeInDogYears = earlyYears + laterYears; //somei earlyYears com laterYears e atribui a minha idade de cachorro
+console.log(myAgeInDogYears)//printei minha idade em cachorro
+let myName = ('Cauê'.toLowerCase());//adicionei uma função que deixa meu nome em minúsculo
+console.log(`Meu nome é ${myName}. Tenho ${myAge} anos em anos humanos, que são ${myAgeInDogYears} anos em anos de cão.`) //criei uma concatenação das três variavéis que eu criei
+
+
